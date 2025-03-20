1150db2c4b7885006ed8ac67fd4e38d239e67d65
sungwoo <p7373993@gmail.com>
2025-03-20 15:10:16 +0900

    백준 25-3-20

--- 11653.cpp
+++ 11653.cpp
@@ -7,8 +7,21 @@ int main()
 {
 	int N;
 	cin >> N;
+	vector<int> vec;
 	while (1)
 	{
-
+		if (N == 1) break;
+		for (int i = 2; i <= N; i++)
+		{
+			if (N % i == 0) {
+				vec.push_back(i);
+				N /= i;
+				break;
+			}
+		}
+	}
+	for (auto& v : vec)
+	{
+		cout << v << endl;
 	}
 }
