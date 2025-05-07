2c0cbd238b1379ed789c64941dc583c31750f789
sungwoo <p7373993@gmail.com>
2025-05-08 01:29:10 +0900

    0508

--- 2580.cpp
+++ 2580.cpp
@@ -1,71 +1,72 @@
 //구멍이 하나인곳부터 시작??
 //행열 단위로 구멍 하나 채우고 그다움에 네모 안 채우기
+//교집합을 통해 나온 값으로 비교해서 하기
 #include <iostream>
 #include <queue>
 #include <vector>
 #include <set>
-
+#include <algorithm> 
+//set_intersection 사용
 using namespace std;
 int sudocu[9][9];
 int row[9];
 int col[9];
 int square[3][3];
-vector<set<int>> row_v(9,{1,2,3,4,5,6,7,8,9});
-vector<set<int>> col_v(9, { 1,2,3,4,5,6,7,8,9 });
-vector<set<int>> square_v(9, { 1,2,3,4,5,6,7,8,9 });
-
-queue <pair<int, int>> se;
-
-void back()
+vector<set<int>> row_v(9,set<int>{1,2,3,4,5,6,7,8,9});
+vector<set<int>> col_v(9, set<int>{1, 2, 3, 4, 5, 6, 7, 8, 9});
+vector<set<int>> square_v(9, set<int>{ 1,2,3,4,5,6,7,8,9 });
+vector<pair<int, int>> blanks; // 빈 셀 위치 저장
+set<int> get_possible(int a, int b)
 {
-	if (se.size() == 0) return;
+	set<int> temp, possible;
+	set_intersection(row_v[a].begin(), row_v[a].end(),
+		col_v[b].begin(), col_v[b].end(),
+		inserter(temp, temp.begin()));
 
-	auto [a,b] = se.front();
-	se.pop();
-	if (row_v[a].size() == 1)//이렇게 사이즈로 나머지 다바꾸기
-	{
-		for (auto& v : row_v[a])
-		{
-			sudocu[a][b] = v;
-			++row[a];
-			++col[b];
-			++square[a / 3][b / 3];
-			row_v[a].extract(v);
-			col_v[b].extract(v);
-			square_v[a / 3 + b / 3].extract(v);
-		}
-		back();
-	}
-	else if (col[b] == 1)
-	{
-		for (auto& v : col_v[b])
-		{
-			sudocu[a][b] = v;
-			++row[a];
-			++col[b];
-			++square[a / 3][b / 3];
+	set_intersection(temp.begin(), temp.end(),
+		square_v[(a / 3) * 3 + b / 3].begin(), square_v[(a / 3) * 3 + b / 3].end(),
+		inserter(possible, possible.begin()));
 
-		}
-		back();
-	}
-	else if (square[a / 3][b / 3] == 1)
+	return possible;
+}
+bool back(int idx)
+{
+	if (idx == blanks.size())
 	{
-		for (auto& v : square_v[a / 3 + b / 3])
-		{
-			sudocu[a][b] = v;
-			++row[a];
-			++col[b];
-			++square[a / 3][b / 3];
-
-		}
-		back();
+		return true;
 	}
+
+	auto [a, b] = blanks[idx];
 	
-	se.emplace(a, b);
+	set<int> possible = get_possible(a, b);
+
+		for (auto v : possible) {
+			
+			sudocu[a][b] = v;
+			row_v[a].erase(v); 
+			col_v[b].erase(v);
+			square_v[(a / 3) * 3 + b / 3].erase(v);
+
+			if (back(idx+1)) return true;
+
+			sudocu[a][b] = 0;
+			row_v[a].insert(v);
+			col_v[b].insert(v);
+			square_v[(a / 3) * 3 + b / 3].insert(v);
+
+		}
+
+
+	
+	
+	return false;
 }
 int main()
 {
-	for (size_t i = 0; i < 81; i++)
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	for (int i = 0; i < 81; i++)
 	{
 		int num;
 		cin >> num;
@@ -73,21 +74,23 @@ int main()
 		int b = i % 9;
 		if (num == 0)
 		{
-
-			se.emplace( a, b);
-			++row[a];
-			++col[b];
-			++square[a / 3][b / 3];
+			blanks.emplace_back(a, b);
 		}
-		row_v[a].extract(num);
-		col_v[b].extract(num);
-		square_v[a/3 + b/3].extract(num);
-		sudocu[a][b] = num;
+		else 
+		{
+			row_v[a].erase(num);
+			col_v[b].erase(num);
+			square_v[(a / 3)*3 + b /3].erase(num);
+			sudocu[a][b] = num;
+		}
+
 	}
-	back();
+	back(0);
+
 	for (size_t i = 0; i < 81; i++)
 	{
 		if (i % 9 == 0) cout << "\n";
-		cout << sudocu[i / 9][i % 9]<<" ";
+		cout << sudocu[i / 9][i % 9] << " ";
 	}
 }
+
