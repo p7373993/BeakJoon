88acebe0275a2b14b040afcb329a16b4e988004c
sungwoo <p7373993@gmail.com>
2025-05-25 11:49:50 +0900

    0525

--- 9095.cpp
+++ 9095.cpp
@@ -18,7 +18,16 @@ void back(int idx,int n,int sum)
 }
 int main()
 {
-	back(3,4, 0);
+	int n;
+	cin >> n;
+	for (size_t i = 0; i < n; i++)
+	{
+		int num;
+		cin >> num;
+		total = 0;
+		back(3, num, 0);
+		cout << total<<endl;
+	}
 
-	cout << total;
+
 }
