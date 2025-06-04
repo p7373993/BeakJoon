f459200ed2785ef98f9138caa7f00753857b92e0
sungwoo <p7373993@gmail.com>
2025-06-04 09:51:07 +0900

    0604

---
+++ 2579.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+//점화식 질문 게시판 봤음..
+
+//f(n) = f(n-2) + an ,f(n-3) + an-1 + an	
+
+//중간에 점프가 있게 점화식을 짜두면 자동으로 빈칸이 생긴다
+int main()
+{
+	int n;
+	cin >> n;
+	int stairs[301];
+	int dp[301];
+	for (size_t i = 0; i < n; i++)
+	{
+		int a;
+		cin >>a;
+		stairs[i] = a;
+	}
+	dp[0] = stairs[0];
+	dp[1] = stairs[0] + stairs[1];
+	for (size_t i = 2; i < n; i++)
+	{
+		dp[i] = max(dp[n - 2] + stairs[i], dp[n - 3] + stairs[i - 1] + stairs[i]);
+	}
+
+	cout << dp[n - 1];
+}
