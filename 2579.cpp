d3bfe9bc8ad474b02fc7f9b1206a2ac591f79ac8
sungwoo <p7373993@gmail.com>
2025-06-04 11:52:31 +0900

    0604

--- 2579.cpp
+++ 2579.cpp
@@ -7,12 +7,15 @@ using namespace std;
 //f(n) = f(n-2) + an ,f(n-3) + an-1 + an	
 
 //중간에 점프가 있게 점화식을 짜두면 자동으로 빈칸이 생긴다
+
+int stairs[301];
+int dp[301];
+
 int main()
 {
 	int n;
 	cin >> n;
-	int stairs[301];
-	int dp[301];
+
 	for (size_t i = 0; i < n; i++)
 	{
 		int a;
@@ -20,11 +23,12 @@ int main()
 		stairs[i] = a;
 	}
 	dp[0] = stairs[0];
-	dp[1] = stairs[0] + stairs[1];
-	for (size_t i = 2; i < n; i++)
+	if(n>1) dp[1] = stairs[0] + stairs[1];
+	if(n>2) dp[2] = max(stairs[0] + stairs[2],stairs[1]+stairs[2]);
+	for (size_t i = 3; i < n; i++)
 	{
-		dp[i] = max(dp[n - 2] + stairs[i], dp[n - 3] + stairs[i - 1] + stairs[i]);
+		dp[i] = max((dp[i- 2] + stairs[i]), (dp[i - 3] + stairs[i - 1] + stairs[i]));
 	}
 
-	cout << dp[n - 1];
+	cout << dp[n-1];
 }
